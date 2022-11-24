01d507bb954ecf1e4b0ed0b3627bd6a1ec5f8038
Lvasp16rnd <lvasprnd018@gmail.com>
2022-11-24 20:11:30 -0300

    url controll

---
+++ Otis/Python/mysite/mysite/urls.py
@@ -0,0 +1,31 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index', TemplateView.as_view(template_name='index.html')),
+    path('escolha', TemplateView.as_view(template_name='escolha.html')),
+    path('contato', TemplateView.as_view(template_name='contato.html')),
+    path('sobreotis', TemplateView.as_view(template_name='sobreotis.html')),
+    path('chat', TemplateView.as_view(template_name='chat.html')),
+    path('css/', TemplateView.as_view(template_name='style.css')),
+    path('Untitled-1', TemplateView.as_view(template_name='Untitled-1.html')),
+    path('js', TemplateView.as_view(template_name='actions.js')),
+]
