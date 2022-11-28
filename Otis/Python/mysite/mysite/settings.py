aaaa2be5fc58de605d53730790962d1143e39efa
Lvasp16rnd <lvasprnd018@gmail.com>
2022-11-28 14:10:27 -0300

    UPDATE settings

--- Otis/Python/mysite/mysite/settings.py
+++ Otis/Python/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k=@rhpzo*r%72xj#klob73gp4#kuqhygotav3*mpa&*w&4ukgz'
+SECRET_KEY = 'COLOQUE SUA CHAVE DE SEGURANÇA AQUI'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
